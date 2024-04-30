fe1f812d7f01ccb375c16b9c10a47c798b154e9a
Anton81951 <antonbugaichuk@gmail.com>
2024-04-30 12:49:31 +0300

    1

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,23 +2,20 @@ import clsx from "clsx";
 import styles from "./Feedback.module.css";
 import Notification from "../Notification/Notification";
 
-const Feedback = ({ votes }) => {
-    const total = votes.good + votes.neutral + votes.bad;
-  
-    if (total === 0) {
-      return <Notification />;
-    }
-  
-    return (
-      <div className={clsx(styles.fdbContainer)}>
-        <p>Good: {votes.good}</p>
-        <p>Neutral: {votes.neutral}</p>
-        <p>Bad: {votes.bad}</p>
-        <p>Total: {total}</p>
-        <p>Positive: {total === 0 ? 0 : Math.round((votes.good / total) * 100)}%</p>
-      </div>
-    );
-  };
-  
+const Feedback = ({ votes, totalFeedback, positivePercentage }) => {
+  if (totalFeedback === 0) {
+    return <Notification />;
+  }
+
+  return (
+    <div className={clsx(styles.fdbContainer)}>
+      <p>Good: {votes.good}</p>
+      <p>Neutral: {votes.neutral}</p>
+      <p>Bad: {votes.bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {positivePercentage}%</p>
+    </div>
+  );
+};
 
 export default Feedback;
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,49 +1,25 @@
-import { useState, useEffect } from "react";
 import clsx from "clsx";
 import styles from "./Options.module.css";
 
-const Options = ({ onUpdate }) => {
-    const [votes, setVotes] = useState(() => {
-      const storedVotes = JSON.parse(localStorage.getItem("votes"));
-      return storedVotes || { good: 0, neutral: 0, bad: 0 };
-    });
-  
-    const handleVote = (type) => {
-      setVotes((prevVotes) => {
-        const updatedVotes = {
-          ...prevVotes,
-          [type]: prevVotes[type] + 1
-        };
-        localStorage.setItem("votes", JSON.stringify(updatedVotes));
-        return updatedVotes;
-      });
-    };
-  
-    const handleReset = () => {
-      localStorage.removeItem("votes");
-      setVotes({ good: 0, neutral: 0, bad: 0 });
-    };
-  
-    useEffect(() => {
-      onUpdate(votes);
-    }, [votes, onUpdate]);
-  
-    return (
-      <div className={clsx(styles.btnContainer)}>
-        <button className={clsx(styles.btnStyle)} onClick={() => handleVote("good")}>
-          Good
-        </button>
-        <button className={clsx(styles.btnStyle)} onClick={() => handleVote("neutral")}>
-          Neutral
-        </button>
-        <button className={clsx(styles.btnStyle)} onClick={() => handleVote("bad")}>
-          Bad
-        </button>
+const Options = ({ votes, handleVote, totalFeedback, handleReset }) => {
+  return (
+    <div className={clsx(styles.btnContainer)}>
+      <button className={clsx(styles.btnStyle)} onClick={() => handleVote("good")}>
+        Good
+      </button>
+      <button className={clsx(styles.btnStyle)} onClick={() => handleVote("neutral")}>
+        Neutral
+      </button>
+      <button className={clsx(styles.btnStyle)} onClick={() => handleVote("bad")}>
+        Bad
+      </button>
+      {totalFeedback > 0 && (
         <button className={clsx(styles.btnStyle)} onClick={handleReset}>
           Reset
         </button>
-      </div>
-    );
-  };
+      )}
+    </div>
+  );
+};
 
 export default Options;
